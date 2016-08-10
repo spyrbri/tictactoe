e71a244bf0d9f1d01c11435d3aedb9557757adef
Spyros Brilis <spyrbri@gmail.com>
2016-08-10 16:36:05 +0300

    Display a symbol for every tile selection

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem 'twitter-bootstrap-rails'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 gem 'react-rails'
+gem 'underscore-rails'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
--- app/assets/javascripts/components/_move_list.js.jsx
+++ app/assets/javascripts/components/_move_list.js.jsx
@@ -2,8 +2,20 @@ var MoveList = React.createClass({
   render: function() {
     moveList = []
 
+    var symbolForTile = function(tile) {
+      var move = _.findWhere(this.props.moves, { tile: tile })
+
+      if (move != undefined)
+        if (move.position % 2)
+          return 'X'
+        else
+          return 'O'
+      else
+        return null
+    }.bind(this)
+
     for(i=1; i < 10; i++) {
-      moveList.push(<Move key={ i } tile={ i } />)
+      moveList.push(<Move key={ i } tile={ i } symbol={ symbolForTile(i) } />)
     }
 
     return (
