167b96e771169c2b6b45519549b4da4b70ab37e6
Spyros Brilis <spyrbri@gmail.com>
2016-08-10 17:40:20 +0300

    Open the /cable route and connect to Redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.web_socket_server_url = "wss://tictactoeforquiqup.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://tictactoeforquiqup.herokuapp.com', 'http://tictactoeforquiqup.herokuapp.com']
 end
