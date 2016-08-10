5c8103dcc8e4d83b2147e9ffd4a3614f400c0b89
Spyros Brilis <spyrbri@gmail.com>
2016-08-10 16:11:47 +0300

    Add validation for moves

--- app/models/move.rb
+++ app/models/move.rb
@@ -1,3 +1,7 @@
 class Move < ApplicationRecord
   belongs_to :game
+
+  validates :tile, uniqueness: true, presence: true,
+    numericality: { only_integer: true, less_than: 10,
+                    greater_than: 0, message: 'Position limit exceede' }
 end
