bf966a2976489dc713dacc5169a7a907bef28a2a
Spyros Brilis <spyrbri@gmail.com>
2016-08-10 17:16:08 +0300

    Add an image instead of a symbol

--- app/assets/javascripts/components/_move.js.jsx
+++ app/assets/javascripts/components/_move.js.jsx
@@ -1,7 +1,9 @@
 var Move = React.createClass({
   render: function() {
     return (
-      <div className='tile col-xs-4' id={ this.props.tile }> { this.props.symbol } </div>
+      <div className='tile col-xs-4' id={ this.props.tile }>
+        <span className={ this.props.imageClass }/>
+      </div>
     )
   }
 });
--- app/assets/javascripts/components/_move_list.js.jsx
+++ app/assets/javascripts/components/_move_list.js.jsx
@@ -2,20 +2,20 @@ var MoveList = React.createClass({
   render: function() {
     moveList = []
 
-    var symbolForTile = function(tile) {
+    var imageForTile = function(tile) {
       var move = _.findWhere(this.props.moves, { tile: tile })
 
       if (move != undefined)
         if (move.position % 2)
-          return 'X'
+          return 'X_image'
         else
-          return 'O'
+          return 'O_image'
       else
         return null
     }.bind(this)
 
     for(i=1; i < 10; i++) {
-      moveList.push(<Move key={ i } tile={ i } symbol={ symbolForTile(i) } />)
+      moveList.push(<Move key={ i } tile={ i } imageClass={ imageForTile(i) } />)
     }
 
     return (<div>{ moveList }</div>)
