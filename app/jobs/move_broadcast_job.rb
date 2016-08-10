e496d564025030ebeb7fcf0ed3855423cc559b39
Spyros Brilis <spyrbri@gmail.com>
2016-08-10 16:48:12 +0300

    Basic game rules

--- app/jobs/move_broadcast_job.rb
+++ app/jobs/move_broadcast_job.rb
@@ -2,6 +2,11 @@ class MoveBroadcastJob < ApplicationJob
   queue_as :default
 
   def perform(move)
-    ActionCable.server.broadcast('move_channel', move: move)
+    winner = Game.check_winner if move.position >=4
+    tie = (move.position >= 9)
+    ActionCable.server.broadcast('move_channel',
+      move: move, winner: winner, tie: tie)
+
+    Move.destroy_all if winner || tie
   end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,26 @@
 class Game < ApplicationRecord
+  WINNING_COMBINATIONS = [
+    [1,2,3], [4,5,6], [7,8,9],
+    [1,4,7], [2,5,8], [3,6,9],
+    [1,5,9], [3,5,7]
+  ]
+
+  has_many :moves, -> { order("position ASC") }
+
+  def self.check_winner
+    moves = Move.all
+    even_moves = moves.select do |m|
+      m.position.even?
+    end.pluck(:tile)
+
+    odd_moves = moves.select do |m|
+      m.position.odd?
+    end.pluck(:tile)
+
+    WINNING_COMBINATIONS.select do |array|
+      (array - even_moves).empty? || (array - odd_moves).empty?
+    end.any?
+  end
+
   has_many :moves, -> { order("position ASC") }
 end
